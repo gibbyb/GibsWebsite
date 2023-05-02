eb868dc5fe4aea1f11e510b68ce74471192ea977
gib <gib@gib-server.gibbyb.com>
2023-05-02 03:51:25 -0500

    Fixed everything. This is the correct everything for this project as it is literally live.

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 8081;
+const connectToDb = require('./db.js');
 
-app.use(express.static(path.join(__dirname, 'client/public'))); // Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'client/build'))); // Serve static files from the "public" directory
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html'); // Serve the index.html file on the root path
 });
 
+// Test database connection
+connectToDb()
+  .then((db) => {
+    console.log('Connected to the database');
+  })
+  .catch((err) => {
+    console.error('Error connecting to the database:', err.stack);
+  });
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
